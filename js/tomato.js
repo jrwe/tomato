4e7166f2d793748ccfeae77fbdd13cc8c66c9d84
Winus Peng <iwinux@gmail.com>
2011-06-04 01:24:07 +0800

    added code handling events of #clock-switch and #ring-switch

--- js/tomato.js
+++ js/tomato.js
@@ -3,7 +3,7 @@ var minutesToMillsecs = function (minutes) {
 };
 
 const interval = 1000;
-const tomatoDuration = minutesToMillsecs(0.1);
+const tomatoDuration = minutesToMillsecs(0.3);
 const shortBreakDuration = minutesToMillsecs(0.1);
 const longBreakDuration = minutesToMillsecs(0.2);
 
@@ -17,6 +17,7 @@ var Tomato = Backbone.Model.extend({
         this.timerID = null;
         this.currentDuration = 0;
         this.remaining = 0;
+        _.bindAll(this, 'onInterval');
     },
 
     startClock: function () {
@@ -33,10 +34,10 @@ var Tomato = Backbone.Model.extend({
         if (this.remaining <= 0) {
             if (!this.isAtBreak()) {
                 this.tomatoCount++;
-                this.task.save({usedTomato: this.tomatoCount});
+                this.currentTask.save({usedTomato: this.tomatoCount});
             }
 
-            this.atBreak = !atBreak;
+            this.atBreak = !this.atBreak;
             this.stopClock();
             this.trigger('timeUp');
         } else {
@@ -53,20 +54,20 @@ var Tomato = Backbone.Model.extend({
         }
     },
 
-    stopClock: function () {
+    stopClock: function (reset) {
         if (this.timerID) {
-            clearInterval(timerID);
+            clearInterval(this.timerID);
             this.timerID = null;
             this.trigger('clockStop');
 
-            /*if (reset) {
-                remaining = currentDuration;
-                onTimeChange();
-            }*/
+            if (reset) {
+                this.remaining = this.currentDuration;
+            }
         }
     },
 
     isAtBreak: function () { return this.atBreak; },
+    isStarted: function () { return this.timerID; },
 
     getTimeText: function () {
         var date = new Date(this.remaining);
@@ -87,9 +88,18 @@ var Tomato = Backbone.Model.extend({
 var TomatoView = Backbone.View.extend({
     initialize: function () {
         //_.bindAll(this, 'onClockStop', 'onClockStart', 'onTimeUp', 'render');
-        _.bindAll(this, 'render');
+        _.bindAll(this,
+            'render', 'onTimeUp', 'onClockStart',
+            'onClockStop', 'onClockSwitchClick',
+            'stopRing'
+        );
         this.model.bind('taskChange', this.render);
-        this.model.bind('timeUp', this.render);
+        this.model.bind('timeChange', this.render);
+        this.model.bind('timeUp', this.onTimeUp);
+        this.model.bind('clockStart', this.onClockStart);
+        this.model.bind('clockStop', this.onClockStop);
+
+        this.clockSwitchLabel = 'Start Tomato';
     },
 
     render: function (opts) {
@@ -106,12 +116,42 @@ var TomatoView = Backbone.View.extend({
                 usedTomato: currentTask.get('usedTomato'),
                 estimatedTomato: currentTask.get('estimatedTomato'),
                 isTimeUp: opts.isTimeUp,
-                clockSwitchLabel: opts.clockSwitchLabel ? opts.clockSwitchLabel : 'Start Tomato',
+                clockSwitchLabel: this.clockSwitchLabel,
             }));
+
+            this.$('#clock-switch').click(this.onClockSwitchClick);
         } else {
             this.el.html(ich.clockContent({task: null}));
         }
 
         return this;
+    },
+
+    stopRing: function () {
+        //this.ring.pause blah blah
+        this.$('#ring-switch').remove();
+    },
+
+    onClockSwitchClick: function () {
+        if (this.model.isStarted()) {
+            this.model.stopClock(true);
+        } else {
+            this.model.startClock();
+        }
+    },
+
+    onClockStart: function () {
+        this.clockSwitchLabel = 'Stop';
+        this.render();
+    },
+
+    onClockStop: function () {
+        this.clockSwitchLabel = this.model.isAtBreak() ? 'Take a break' : 'Start Tomato';
+        this.render();
+    },
+
+    onTimeUp: function () {
+        this.render({isTimeUp: true});
+        this.$('#ring-switch').click(this.stopRing);
     }
 });
